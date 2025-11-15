134d15c699b998257703d152644c7bd4b0f0e838
John Hobbs <john@velvetcache.org>
2025-11-14 20:29:07 -0600

    Remove template stories, start login screen

---
+++ app/login.tsx
@@ -0,0 +1,59 @@
+import { Link } from 'expo-router';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function LoginScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.header} type="title">Pets In Pyjamas</ThemedText>
+      <TextInput placeholder="Email" style={styles.input} />
+        <TextInput
+          style={styles.input} 
+          secureTextEntry={true}
+          placeholder="Password"
+        />
+      <View style={styles.button} >
+        <Button
+          title="Login"
+          onPress={() => {}}
+        />
+      </View>
+      <Link href="/" dismissTo style={styles.link}>
+        <ThemedText type="link">Sign Up</ThemedText>
+      </Link>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  header: {
+    marginBottom: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  input: {
+    backgroundColor: 'white',
+    marginTop: 15,
+    fontSize: 16,
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '100%',
+  },
+  button: {
+    marginTop: 15,
+    width: '80%',
+  }
+});
+
---
+++ stories/app/LoginScreen.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react-native';
+
+import LoginScreen from '../../app/login';
+
+const meta = {
+  title: 'Screen/Login',
+  component: LoginScreen,
+} satisfies Meta<typeof LoginScreen>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
