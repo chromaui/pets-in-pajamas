1415ac8b02a354af35dda6d0261c8a3f91b5005b
John Hobbs <john@velvetcache.org>
2026-02-04 14:05:01 -0600

    Tidy up, tweak README

--- .rnstorybook/main.ts
+++ .rnstorybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-native';
 
 const main: StorybookConfig = {
+  framework: '@storybook/react-native',
   stories: ['../stories/**/*.stories.?(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
 };
