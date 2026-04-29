768e4869f4fb96901dfca9069c85470a49136d37
John Hobbs <john@velvetcache.org>
2026-04-29 09:58:39 -0500

    Use websocket configuration values from environment.

--- .rnstorybook/index.ts
+++ .rnstorybook/index.ts
@@ -6,6 +6,11 @@ const StorybookUIRoot = view.getStorybookUI({
     getItem: AsyncStorage.getItem,
     setItem: AsyncStorage.setItem,
   },
+  enableWebsockets: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true',
+  host: process.env.EXPO_PUBLIC_STORYBOOK_WEBSOCKET_HOST || 'localhost',
+  port: parseInt(process.env.EXPO_PUBLIC_STORYBOOK_WEBSOCKET_PORT || '7007', 10),
+  secured: process.env.EXPO_PUBLIC_STORYBOOK_WEBSOCKET_SECURED === 'true',
+  onDeviceUI: process.env.EXPO_PUBLIC_STORYBOOK_DISABLE_UI !== 'true',
 });
 
 export default StorybookUIRoot;
--- .rnstorybook/main.ts
+++ .rnstorybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/react-native';
 const main: StorybookConfig = {
   framework: '@storybook/react-native',
   stories: ['../stories/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
+  addons: [],
 };
 
 export default main;
