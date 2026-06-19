55474d51db2eb7ba95cbe3e5924977f28f826c33
Mengqi Zhu <mengqi@chromatic.com>
2026-06-19 19:37:27 -0400

    fix storybook rendering on web

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,10 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import type { StorybookConfig } from '@storybook/react-native-web-vite';
+import type { InlineConfig } from 'vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const expoModulesCoreMock = path.resolve(__dirname, './mocks/expo-modules-core.js');
 
 const config: StorybookConfig = {
   "stories": [
@@ -8,6 +14,18 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-native-web-vite",
     "options": {}
-  }
+  },
+  async viteFinal(config: InlineConfig) {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          'expo-modules-core': expoModulesCoreMock,
+        },
+      },
+    };
+  },
 };
 export default config;
