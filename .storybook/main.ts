d9f066b2d76622757b1dad49a738b47fc9b7b1eb
Mengqi Zhu <mengqi@chromatic.com>
2026-06-24 09:16:45 -0400

    update storybook settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,31 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import type { StorybookConfig } from '@storybook/react-native-web-vite';
+import type { InlineConfig } from 'vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const expoModulesCoreMock = path.resolve(__dirname, './mocks/expo-modules-core.js');
 
 const config: StorybookConfig = {
   "stories": [
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [],
+  "addons": ["@chromatic-com/storybook"],
   "framework": {
     "name": "@storybook/react-native-web-vite",
     "options": {}
-  }
+  },
+  async viteFinal(config: InlineConfig) {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          'expo-modules-core': expoModulesCoreMock,
+        },
+      },
+    };
+  },
 };
 export default config;
