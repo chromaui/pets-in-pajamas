101bb3545dfaf5c9805e49ce0f13b57d5acc0cc7
Mengqi Zhu <mengqi@chromatic.com>
2026-07-02 17:16:16 -0400

    Tweak login error story punctuation

--- stories/app/LoginScreen.stories.tsx
+++ stories/app/LoginScreen.stories.tsx
@@ -15,7 +15,7 @@ export const Default: Story = {};
 
 export const Error: Story = {
   args: {
-    error: 'Invalid email or password. Please try again!',
+    error: 'Invalid email or password. Please try again.',
   },
 };
 
